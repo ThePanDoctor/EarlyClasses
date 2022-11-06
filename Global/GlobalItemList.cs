73ff6676656081fe73f6eb8708b94abf862bbd18
Sebastian-Devus <86898507+SebastianDevus@users.noreply.github.com>
2022-11-06 11:16:55 -0300

    Finishes Flinx Fur Greaves, adds Flinx Fur Headpiece and localization
    Should've done separate commits but got a little excited making stuff

--- Global/GlobalItemList.cs
+++ Global/GlobalItemList.cs
@@ -1,6 +1,7 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using EarlyClasses.Items;
 
 namespace EarlyClasses.Global
 {
@@ -14,7 +15,7 @@ namespace EarlyClasses.Global
 			}
 		}
 
-		public override void AddRecipes() // Adds a cheaper recipe to the Flinx Fur Coat
+		public override void AddRecipes() 
 		{
 			Recipe.Create(ItemID.FlinxFurCoat, 1)
 			.AddIngredient(ItemID.GoldBar, 8)
@@ -22,7 +23,8 @@ namespace EarlyClasses.Global
 			.AddIngredient(ItemID.Silk, 5)
 			.AddTile(TileID.Anvils)
 			.Register();
-
+			// Adds cheaper recipes to the Flinx Fur Coat
+			// Above uses gold, below uses platinum
 			Recipe.Create(ItemID.FlinxFurCoat, 1)
 			.AddIngredient(ItemID.PlatinumBar, 8)
 			.AddIngredient(ItemID.FlinxFur, 3)
@@ -39,10 +41,16 @@ namespace EarlyClasses.Global
 			{
 				return "GoldSet";
 			}
-
+			
+			// Checking for platinum armor pieces
 			if (head.type == ItemID.PlatinumHelmet && body.type == ItemID.PlatinumChainmail && legs.type == ItemID.PlatinumGreaves) 
 			{
 				return "PlatinumSet";
+			}
+
+			if (head.type == ModContent.ItemType<FlinxFurHeadpiece>() && body.type == ItemID.FlinxFurCoat && legs.type == ModContent.ItemType<FlinxFurGreaves>())
+			{
+				return "FlinxSet";
 			}
 			
 			// In case no if passes
@@ -53,9 +61,20 @@ namespace EarlyClasses.Global
 		{
 			if(set == "GoldSet" || set == "PlatinumSet") 
 			{
-				// If golden or platinum set is worn, add 10% melee damage.
-				player.setBonus += "\nIncreases melee damage by 10%";
-				player.GetDamage(DamageClass.Melee) += 0.1f;
+				// If golden or platinum set is worn, add 15% melee damage.
+				player.setBonus += "\nIncreases melee damage by 15%";
+				player.GetDamage(DamageClass.Melee) += 0.15f;
+			}
+
+			if (set == "FlinxSet")
+			{
+				// If the full set is worn, get immunity to chilled and frozen, and gain 10% whip speed
+				player.setBonus = "Increases whip speed by 10%\nGrants immunity to Frozen and Chilled";
+				
+				player.buffImmune[BuffID.Chilled] = true;
+				player.buffImmune[BuffID.Frozen] = true;
+
+				player.GetAttackSpeed(DamageClass.SummonMeleeSpeed) += 0.1f;
 			}
 		}
 	}
--- Items/FlinxFurHeadpiece.cs
+++ Items/FlinxFurHeadpiece.cs
@@ -5,12 +5,14 @@ using Terraria.GameContent.Creative;
 
 namespace EarlyClasses.Items
 {
-	[AutoloadEquip(EquipType.Legs)]
-	public class FlinxFurGreaves : ModItem
+	[AutoloadEquip(EquipType.Head)]
+	public class FlinxFurHeadpiece : ModItem
 	{
 		public override void SetStaticDefaults()
 		{
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+			ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true;
 		}
 
 		public override void SetDefaults()
@@ -21,33 +23,30 @@ namespace EarlyClasses.Items
 			Item.value = Item.buyPrice(gold: 2);
 			Item.rare = ItemRarityID.Green;
 
-			Item.defense = 2;
+			Item.defense = 1;
 		}
 
 		public override void UpdateEquip(Player player)
 		{
-			// Increases move speed by 10%
-			player.moveSpeed += 0.1f;
-
-			// Increase summon damage by 10%
-			player.GetDamage(DamageClass.Summon) += 0.1f;
+			// Increase whip speed by 5%
+			player.GetAttackSpeed(DamageClass.SummonMeleeSpeed) += 0.05f;
 		}
 
 		public override void AddRecipes()
 		{
 			// Recipe with gold
 			CreateRecipe() 
-				.AddIngredient(ItemID.GoldBar, 6)
-				.AddIngredient(ItemID.Silk, 5)
-				.AddIngredient(ItemID.FlinxFur, 2)
+				.AddIngredient(ItemID.GoldBar, 3)
+				.AddIngredient(ItemID.Silk, 2)
+				.AddIngredient(ItemID.FlinxFur, 1)
 				.AddTile(TileID.Anvils)
 				.Register();
 
 			// Same but with platinum
 			CreateRecipe()
-				.AddIngredient(ItemID.PlatinumBar, 6)
-				.AddIngredient(ItemID.Silk, 5)
-				.AddIngredient(ItemID.FlinxFur, 2)
+				.AddIngredient(ItemID.PlatinumBar, 3)
+				.AddIngredient(ItemID.Silk, 2)
+				.AddIngredient(ItemID.FlinxFur, 1)
 				.AddTile(TileID.Anvils)
 				.Register();
 		}
